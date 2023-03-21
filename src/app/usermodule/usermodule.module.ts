ffc59bdb6e8b7dae4d8dcf7464e109af414d17eb
lakshmi <lakshmirkrishna7@gmail.com>
2023-03-21 13:13:40 +0530

    initial commit

---
+++ src/app/usermodule/usermodule.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserlandingComponent } from './userlanding/userlanding.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserlandingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsermoduleModule { }
